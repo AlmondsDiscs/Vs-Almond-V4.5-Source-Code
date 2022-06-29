73e6f3708d20790ef79be1e7da5a38f12e5dcc60
AlmondsDemo <90089034+AlmondsDiscs@users.noreply.github.com>
2022-06-29 17:53:26 +0800

    morbius

--- example_mods/data/interfere/cutscene.lua
+++ example_mods/data/interfere/cutscene.lua
@@ -1,9 +1,11 @@
 local cutsceneEnded = false
 function onEndSong()
 	if isStoryMode and not cutsceneEnded then
-		startVideo('greatjob');
-		allowCountdown = true;
-		return Function_Stop;
+		if getProperty('ratingPercent') > 1 then
+			startVideo('greatjob');
+			allowCountdown = true;
+			return Function_Stop;
+		end
 	end
 	return Function_Continue;
 end
